8e752e19dcaec1d9e394ea5d0c9d518ac9a66d00
masol <cerfnsufyljy2001@mail.ru>
2023-10-24 20:00:49 +0400

    первый

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,21 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (el) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const placesList = document.querySelector('.places__list');
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    placesItem.querySelector('.card__image').src = el.link;
+    placesItem.querySelector('.card__title').textContent = el.name;
+    placesList.append(placesItem); 
+});
+
+let deleteCard = document.querySelectorAll('.card__delete-button');
+deleteCard.forEach((elem)=>{
+  elem.addEventListener('click', deleteParent);
+});
+function deleteParent(){
+    let deleteElement = this.parentElement;
+    deleteElement.remove();
+}
