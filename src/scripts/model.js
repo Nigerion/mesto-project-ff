a9f67f5fab40b7a437dad34da0d1b5aba7180dcb
masol <cerfnsufyljy2001@mail.ru>
2023-11-25 23:16:27 +0400

    третий

--- src/scripts/model.js
+++ src/scripts/model.js
@@ -1,15 +1,15 @@
 export function openModal(pop){
   pop.classList.add('popup_is-opened');
-  document.addEventListener('keydown', function ecs(e){escape(e,pop)});
+  document.addEventListener('keydown',escape);
 }
 
 export function closeModal(pop){
   pop.classList.remove('popup_is-opened');
-  document.removeEventListener('keydown', function ecs(e){escape(e)});
+  document.removeEventListener('keydown',escape);
 }
 
 export function escape(e,pop){
-  if (e.key === "Escape"&& pop.classList.contains('popup_is-opened')) {
+  if (e.key === "Escape") {
     const popup = document.querySelector('.popup_is-opened');
     closeModal(popup);
   }
